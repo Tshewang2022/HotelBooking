ef0b474995b49ae382f6e70569e42019c4dcec73
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-04-16 23:21:12 +0600

    it was supposed to be completed today, but got some urgent work

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>this will be main bar</div>;
+};
+
+export default App;
---
+++ src/constants/Index.ts
@@ -0,0 +1,58 @@
+export const reseravtions = [
+  {
+    imgUrl: "",
+    title: "MOUNTAIN HOTEL",
+  },
+  {
+    imgUrl: "",
+    title: "ISLAND RESORT",
+  },
+  {
+    imgUrl: "",
+    title: "CITY APARTHOTEL",
+  },
+  {
+    imgUrl: "",
+    title: "MOUNTAIN CHALET",
+  },
+  {
+    imgUrl: "",
+    title: "COUNTRYSIDE LODGE",
+  },
+];
+
+export const customerSupport = [
+  {
+    imgIcon: "",
+    title: "Install & Import",
+  },
+  {
+    imgIcon: "",
+    title: "Customize Site",
+  },
+  {
+    imgIcon: "",
+    title: "Ready & Launch",
+  },
+];
+
+export const contents = [
+  {
+    imgIcon: "",
+    title: "Free Lifetime Updates",
+    descriptions:
+      "Our products are constantly updated to adapt to higer standards and industry trends",
+  },
+  {
+    imgIcon: "",
+    title: "Flexibility & Customizability",
+    descriptions:
+      "Close, fonts and more website appearance options cann be customized with instant preview",
+  },
+  {
+    imgIcon: "",
+    title: "Professional Customer Support",
+    descriptions:
+      "Receive one-on-one technique support services from our 5-star rating support team",
+  },
+];
