8416b7016d4b6aef899aa296666d627b4862a4f2
Tshewang2022 <tshewanggyaltshen0@gmail.com>
2024-04-19 21:25:50 +0600

    updated

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,5 +1,7 @@
 const Hero = () => {
-  return <div>Updating, havent touched today</div>;
+  return (
+    <div>Updating, havent touched today, havent dont any projects today</div>
+  );
 };
 
 export default Hero;
